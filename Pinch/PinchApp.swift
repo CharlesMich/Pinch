d31f3bce57ab4f74693ae3533414ff3b84714ae7
CharlesMich <charleedongo@hotmail.com>
2024-08-18 22:53:22 -0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Charles Michael on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
